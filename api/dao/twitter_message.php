f020817bf89305fb246390ef05159e502d281dc6
Jeff Standen <jeff@webgroupmedia.com>
2018-05-25 16:36:49 -0700

    9.0 compatibility

--- api/dao/twitter_message.php
+++ api/dao/twitter_message.php
@@ -630,7 +630,7 @@ class View_TwitterMessage extends C4_AbstractView implements IAbstractView_Subto
 				
 			default:
 				// Custom fields
-				if('cf_' == substr($column,0,3)) {
+				if(DevblocksPlatform::strStartsWith($column, 'cf_')) {
 					$counts = $this->_getSubtotalCountForCustomColumn($context, $column);
 				}
 				
